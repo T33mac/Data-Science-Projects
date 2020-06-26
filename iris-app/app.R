e18f8a70dcfe43a08f9371149444d872e150ab50
T33mac <31869841+T33mac@users.noreply.github.com>
2020-06-26 11:13:34 -0700

    finished p. 193

--- iris-app/app.R
+++ iris-app/app.R
@@ -11,16 +11,26 @@ ui <- fluidPage(
   sidebarLayout(
     
     sidebarPanel(
+      h4("Predict an iris's sepal length from a prediction model using petal length on a slider"),
       
       sliderInput(inputId = "length",
                   label = "Petal Length in Inches",
                   min = min(iris$Petal.Length),
                   max = max(iris$Petal.Length),
-                  value = mean(iris$Petal.Length))
+                  value = mean(iris$Petal.Length)),
+      br(),
+      br(),
+      h4("Diagram of a ",span("Blue Flag Iris", style = "color:blue")),
+      img(src = "blueflagiris_flower.jpg", height = 190, width = 220)
       
     ),
     
-    mainPanel(plotOutput("plotted"))
+    mainPanel(
+      h1("Prediction Plot"),
+      br(),
+      plotOutput("plotted"),
+      h3("Predicted sepal length in inches"),
+      h4(textOutput("texted")))
   )
 )
 
@@ -50,6 +60,15 @@ server <- function(input, output) {
     points(x=p_length, y=s_length, #Don't know what to put for y here to follow the regression line
            col = "red",
            lwd = 6)
+  })
+  output$texted <- renderText({
+    p_length <- data.frame(input$length)
+    
+    new.data <- data.frame(Petal.Length = input$length)
+    
+    s_length <- predict(model, newdata = new.data)
+    
+    print(s_length)
   })
 }
 ?points
